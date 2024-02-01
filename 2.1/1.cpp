d048c94906a5ef9929c78db124e33d548fcf305f
Deep-Gohil <deep72617626@gmail.com>
2024-02-01 19:11:35 +0530

    2.1

---
+++ 2.1/1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a=12,b=8;
+	
+	cout << "Addition Of " << a << " And " << b << " Is : " << a+b << endl;
+	cout << "Substraction Of " << a << " And " << b << " Is : " << a-b << endl;
+	cout << "Multiplication Of " << a << " And " << b << " Is : " << a*b << endl;
+	cout << "Division Of " << a << " And " << b << " Is : " << a/b << endl;
+	cout << "Modulas Of " << a << " And " << b << " Is : " << a%b << endl; 
+	
+	return 0;
+}
